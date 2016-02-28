e65197e71b16e64299146abf0c34f7dd392d1d5e
khanhnhat <nguyen.k21@gmail.com>
2016-02-27 21:39:31 -0500

    Wrote code to display meals

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,31 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+let getMeals= function(){
+  $.ajax({
+    url: "http://localhost:3000/meals",
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(response){
+    console.log('Get request');
+    displayMeals(response);
+  });
+};
+
+let displayMeals = function(response){
+  let meals = response.meals;
+  let mealListingTemplate = require('./meal-listing.handlebars');
+      $('.meals').append(mealListingTemplate({
+          meals
+        }));
+
+  console.log(response.meals);
+};
+
+$(document).ready(function(){
+  $("#rest1").on('click',function(){
+    getMeals();
+      $('.meals').show();
+  });
+});
