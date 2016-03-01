5a76fbd5c6a3e9a1fb1a99a599c00dbbf4b961de
khanhnhat <nguyen.k21@gmail.com>
2016-03-01 15:13:54 -0500

    Add functions to javascript file

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,28 +4,54 @@
 // var example = require('./example');
 
 // use require without a reference to ensure a file is bundled
-require('./example');
+
+// const myApp = {
+//   baseURL: 'http://localhost:3000'
+// };
+
 let getMealsApi= require('./getMeals');
-let userApi= require('./user-api');
-
-
-
+let userApi = require('./user-api');
+require('./example');
+let createReview = require('./create-review');
+let deleteReview = require('./delete-review');
+let updateReview = require('./update-review');
 
 
 $(document).ready(function(){
   userApi.init();
   $('.menu').hide();
 
-   $("#rest1").on('click',function(){
+   $("#1").on('click',function(e){
+     e.preventDefault();
      getMealsApi.getLunch();
      getMealsApi.getBreakfast();
+     getMealsApi.getDinner();
        $('.homepage').hide();
        $('.menu').show();
    });
 
-  $(".navbar-brand").on('click',function(){
-      $('.homepage').show();
-      $('.menu').hide();
+  $(".navbar-brand").on('click',function(e){
+    e.preventDefault();
+    $('.homepage').show();
+    $('.menu').hide();
   });
 
+
+
+});
+
+$("#review-form").on('submit', function(e){
+  e.preventDefault();
+  createReview.createReview(e);
+});
+
+
+$("#delete-review-form").on('submit', function(e){
+  e.preventDefault();
+  deleteReview.deleteReview(e);
+});
+
+$("#update-review-form").on('submit', function(e){
+  e.preventDefault();
+  updateReview.updateReview(e);
 });
