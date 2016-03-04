6bbca49d3cdce7b15e0a16bc31640761af5e5bd5
khanhnhat <nguyen.k21@gmail.com>
2016-03-04 16:37:33 -0500

    refactor code

--- assets/scripts/update-review.js
+++ assets/scripts/update-review.js
@@ -1,33 +1,27 @@
 'use strict';
 
-let userApi= require('./user-api');
+var userApi= require('./user-api');
 
 
-//
-let updateReview = function(e) {
-  // debugger;
+var updateReview = function(e) {
   e.preventDefault();
-  console.log("update button works");
-  let reviewID = $('.update-review-button').attr('data-review-id');
-  console.log('update review id: ' + reviewID);
-
-  let formData = new FormData(e.target);
+  var reviewID = $('.update-review-button').attr('data-review-id');
+  var formData = new FormData(e.target);
   $.ajax({
     url: userApi.myApp.baseURL + "/reviews/" +reviewID,
-      method: 'PATCH',
-      headers: {
-        Authorization: 'Token token=' + userApi.myApp.user.token,
-      },
-      contentType: false,
-      processData: false,
-      data: formData,
-    }).done(function(data) {
-      console.log("update review");
-      $('#update-review').modal('hide');
-    }).fail(function(jqxhr) {
-      console.error(jqxhr);
-    });
-  };
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + userApi.myApp.user.token,
+    },
+    contentType: false,
+    processData: false,
+    data: formData,
+  }).done(function(data) {
+    $('#update-review').modal('hide');
+  }).fail(function(jqxhr) {
+    console.error(jqxhr);
+  });
+};
 
 
 module.exports = {
